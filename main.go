beafb59f2b027ecfc281ebed4fe3ff928a7dc4cd
jkosik <juraj.kosik@gmail.com>
2022-07-30 22:08:00 +0200

    further testing

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 	//chartPath := "samples/hyperbridge-1.1.300.tgz"
 	//chartPath := "samples/hyperbridge-data-1.0.160.tgz"
 	chartPath := "samples/nginx-12.0.0.tgz"
+	//chartPath := "samples/haproxy-0.3.25.tgz"
 
 	chart, err := loader.Load(chartPath)
 	if err != nil {
@@ -20,21 +21,12 @@ func main() {
 	}
 
 	fmt.Println("\n===== Helm Chart ===========")
-	fmt.Print(*chart)
-	engine := engine.Engine{Strict: false, LintMode: false}
+	fmt.Print(chart)
 
-	// Prepare vals
-	var vals map[string]interface{}
-	// To override values
-	// vals := map[string]interface{}{
+	var vals chartutil.Values
+	// To override Chart values
+	// vals := chartutil.Values{
 	// 	"replicaCount": 3,
-	// }
-
-	// Readvalues
-	// var myData []byte
-	// d, err := chartutil.ReadValues(myData)
-	// if err != nil {
-	// 	panic(err)
 	// }
 
 	// Coalesce values
@@ -45,12 +37,15 @@ func main() {
 	}
 
 	fmt.Println("\n===== Helm Values - raw ===========")
-	fmt.Println(reflect.TypeOf(coalescedVals))
+	fmt.Println(reflect.TypeOf(coalescedVals.AsMap())) // map[string]interface {}
+	fmt.Println(reflect.TypeOf(coalescedVals))         // chartutil.Values
 	fmt.Println(coalescedVals)
-	fmt.Println("\n===== Helm Values - in Yaml ===========")
+	fmt.Println("===== Helm Values - Yaml ===========")
 	fmt.Println(coalescedVals.YAML())
-	fmt.Println("\n====== Helm Templating ==========")
-	// func (e Engine) Render(chrt *chart.Chart, values chartutil.Values) (map[string]string, error)
-	fmt.Println(engine.Render(chart, coalescedVals))
 
+	fmt.Println("\n====== Helm Templating ==========")
+	//e := engine.Engine{Strict: false, LintMode: false}
+	var e engine.Engine
+	//fmt.Println(e.Render(chart, vals))
+	fmt.Println(e.Render(chart, coalescedVals))
 }
